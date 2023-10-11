d21fd8c7f9cac2d5051a1ce7914c6a4490439f9a
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-10-11 11:09:26 +0300

    11.10.2023

---
+++ app/src/main/java/com/example/unte2/uyg6.java
@@ -0,0 +1,14 @@
+package com.example.unte2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uyg6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uyg6);
+    }
+}

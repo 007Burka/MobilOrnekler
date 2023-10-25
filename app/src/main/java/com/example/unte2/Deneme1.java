3a51c74420e082b6b8153a97cdbd55963ab4f41c
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-10-25 11:23:11 +0300

    25,10.2023

---
+++ app/src/main/java/com/example/unte2/Deneme1.java
@@ -0,0 +1,14 @@
+package com.example.unte2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Deneme1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deneme1);
+    }
+}
